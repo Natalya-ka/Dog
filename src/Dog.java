6392f94543e57f812cb69218edbde0e3a64d4c6c
Natalya-ka <93324362+Natalya-ka@users.noreply.github.com>
2021-10-29 12:14:00 +0200

    messege

---
+++ src/Dog.java
@@ -0,0 +1,35 @@
+public class Dog {
+    private String name;
+    private int age;
+    public Dog(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public Dog(){
+
+    }
+    public void bark(String woof){
+        System.out.println("Woof");
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public static void main(String[]args){
+        Dog tom=new Dog("Tom",5);
+        tom.setAge(6);
+        tom.bark("woof");
+        System.out.println("Age Tom-"+tom.getAge()+"years");
+
+
+    }
+}
+
